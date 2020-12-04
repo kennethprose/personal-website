5dd17a796101e14a844c3d60cb58680f3a1786a9
Kenneth <kenneth.rose@stonybrook.edu>
2020-12-03 21:57:28 -0500

    Added splash screen background and removed the navbar that I started and never finished lol

--- personal-website/src/Components/SplashScreen.js
+++ personal-website/src/Components/SplashScreen.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-rotating-text'
+import icons from '../Triple_Black.png'
 var ReactRotatingText = require('react-rotating-text');
 
 function SplashScreen() {  
     return (
         <div className="Panel" style={{height: "100vh"}}>
-            <Row className="no-gutters" style={{backgroundColor: "#212121", height: "100%"}}>
+            <Row className="no-gutters" style={{backgroundImage: 'url('+icons+')', height: "100%"}}>
                 <Col sm={2} />
                 <Col sm={8}>
-                    <h1 style={{color: "white", fontSize: "150px", paddingTop: "10%"}}>Hello!</h1>
+                    <h1 style={{color: "white", fontSize: "150px", paddingTop: "15%"}}>Hello!</h1>
                     <h2 style={{color: "white", fontSize: "45px", paddingLeft: "9px"}}>I guess you're hear to learn about Kenny's <ReactRotatingText items={['education.', 'past projects.', 'contact info.']} /></h2> 
                 </Col>
                 <Col sm={2} />
