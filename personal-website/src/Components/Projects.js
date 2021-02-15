01129fdc93ac5a206f1e6a579227144ac6677cd6
Kenneth <kenneth.rose@stonybrook.edu>
2021-02-15 03:09:40 -0500

    Template for projects

--- personal-website/src/Components/Projects.js
+++ personal-website/src/Components/Projects.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import icons from '../Triple_Black.png'
+import temp from '../temp.jpg'
+import { Row } from 'react-bootstrap';
+import { GitHub } from 'react-feather';
+import Button from 'react-bootstrap/Button'
 
 function Projects() {
     return (
-        <div className="Panel" style={{height: "100vh", backgroundColor: "darkgrey"}}>
-            
+        <div className="Panel" style={{height: "100vh"}}>
+            <Row className="no-gutters" style={{ display: "flex", alignItems: "center", justifyContent: "center", backgroundImage: 'url('+icons+')', backgroundAttachment: "fixed", height: "100%" }}>
+                <div className="card">
+                    <img className="card_pic" src={temp} alt="A preview of the propject"></img>
+                    <h2 style={{ textAlign: "center", lineHeight: "125%", marginTop: "5%" }}><u>Title</u></h2>
+                    <h4 style={{ textAlign: "center", lineHeight: "125%", marginTop: "5%" }}>This is the description of the project that will provide a brief summary of the project.</h4>
+                    <Button variant="outline-dark" href="https://github.com/kennethprose" target="_blank" className="project_github" style={{ marginTop: "5%" }}><GitHub></GitHub></Button>
+                </div>
+                <div className="card">
+                    
+                </div>
+            </Row>
         </div>
     )
 }
