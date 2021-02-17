7a4ef6b82f41ac7fe630409f86fb37917c56c397
Kenneth <kenneth.rose@stonybrook.edu>
2021-02-17 03:02:22 -0500

    Removed build from gitignore to easily transfer code to web server

---
+++ personal-website/build/precache-manifest.591e8e73678033f79bb288f18ba51a2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d628c7b30ae99354406fe945df48671f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1eea30d38fac25dda8",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b6cbf1eb517683e3ee7a",
+    "url": "/static/css/main.ed46f88f.chunk.css"
+  },
+  {
+    "revision": "6b1eea30d38fac25dda8",
+    "url": "/static/js/2.4df7a158.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4df7a158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cbf1eb517683e3ee7a",
+    "url": "/static/js/main.5b2a157f.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "d90ac6efd32f315aeda50146a9085fb2",
+    "url": "/static/media/Personal-Website.d90ac6ef.png"
+  },
+  {
+    "revision": "f52a0e7e973767101d60217e8ad702b9",
+    "url": "/static/media/Profile_Pic.f52a0e7e.jpg"
+  },
+  {
+    "revision": "001b77fb6e31ad99d5d925aac43d26a3",
+    "url": "/static/media/Set_Cover.001b77fb.png"
+  },
+  {
+    "revision": "e16eedbd92b151613ecc3e1d5365ee19",
+    "url": "/static/media/ToDo-Website.e16eedbd.png"
+  },
+  {
+    "revision": "2efb9fb6c221856c2afd7718787c4c7b",
+    "url": "/static/media/Triple_Black.2efb9fb6.png"
+  }
+]);
